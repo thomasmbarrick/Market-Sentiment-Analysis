21ef8c9b5e2291ea7afbfcfde5112c366a923e79
Thomas Barrick <thomasmbarrick@gmail.com>
2024-07-03 17:55:54 +0100

    data manipulation

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ stock_symbols = ["AMD", "NVDA", "INTC"]
 
 
 news_data = {}
+final_data = []
 for stock in stock_symbols:
     url = base_url + stock
     
@@ -14,7 +15,32 @@ for stock in stock_symbols:
     
     html = BeautifulSoup(response, "html")
     news_data[stock] = html.find(id="news-table")
+    data_rows = news_data[stock].findAll("tr")
     
+    stock_data = []
+    for index, row in enumerate(data_rows):
+        
+        article_title = row.a.text 
+        
+        timestamp_raw = row.td.text
+        timestamp = timestamp_raw.replace("\r\n", "")
+        timestamp_split = timestamp.split(" ")
+        
+        timestamp_split = list(filter(None, timestamp_split))
+        
+        if len(timestamp_split) == 1:
+            article_time = timestamp_split[0]
+        else:
+            article_date = timestamp_split[0]
+            article_time = timestamp_split[1]
+
+            
+        stock_data.append([stock, article_date, article_time, article_title])
+        
+    final_data.append(stock_data)
     break
 
-print(news_data)
+        
+#print(final_data)
+
+
